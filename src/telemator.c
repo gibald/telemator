281f2c8bad9becf794bcf26aa10d720ec9bd0b85
Gibald <git@gibald.com>
2015-07-08 12:08:39 +0100

    add advanced squeezebox menu

--- src/telemator.c
+++ src/telemator.c
@@ -9,6 +9,7 @@
 
 static Window *s_main_window;
 static Window *windows_squeezebox;
+static Window *window_advance_squeezebox;
 static TextLayer *text_layer;
 static TextLayer *s_time_layer;
 static TextLayer *s_weather_layer;
@@ -20,12 +21,20 @@ static SimpleMenuSection s_menu_sections[NUM_MENU_SECTIONS];
 static SimpleMenuItem s_first_menu_items[NUM_FIRST_MENU_ITEMS];
 static SimpleMenuItem s_second_menu_items[NUM_SECOND_MENU_ITEMS];
 static GBitmap *s_menu_icon_image;
+
 static ActionBarLayer *action_bar;
 
 static GFont s_weather_font;
 
 static bool s_special_flag = false;
 static int s_hit_count = 0;
+
+// Advance SqueezeBox Menu
+static SimpleMenuLayer *s_advance_squeezebox_menu_layer;
+static SimpleMenuSection s_advance_squeezebox_menu_sections[NUM_MENU_SECTIONS];
+static SimpleMenuItem s_advance_squeezebox_first_menu_items[NUM_FIRST_MENU_ITEMS];
+static SimpleMenuItem s_advance_squeezebox_second_menu_items[NUM_SECOND_MENU_ITEMS];
+
 
 void send_int(char* key, char* cmd)
 {
@@ -42,23 +51,24 @@ void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(s_time_layer, "play");
     send_int("sb", "play");
 }
+void select_long_click_handler(ClickRecognizerRef recognizer, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "LOoooooong SELECT");
+  window_stack_push(window_advance_squeezebox, true);
+}
+void select_long_click_release_handler(ClickRecognizerRef recognizer, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "LOoooooong SELECT release");
+}
 
 void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   APP_LOG(APP_LOG_LEVEL_DEBUG, "click UP");
-  text_layer_set_text(s_time_layer, "pause");
-    send_int("sb", "pause");
+  text_layer_set_text(s_time_layer, "previous");
+    send_int("sb", "play");
 }
 
 void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   APP_LOG(APP_LOG_LEVEL_DEBUG, "click DOWN");
-  text_layer_set_text(s_time_layer, "Down");
+  text_layer_set_text(s_time_layer, "next");
     send_int("sb", "dddd");
-}
-void back_click_handler(ClickRecognizerRef recognizer, void *context) {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "BACKK");
-  text_layer_set_text(s_time_layer, "BACKK");
-  // window_stack_push(s_main_window, true);
-  window_stack_pop(true);
 }
 
 static void menu_select_callback(int index, void *ctx) {
@@ -67,9 +77,10 @@ static void menu_select_callback(int index, void *ctx) {
 }
 
 void click_config_provider2(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, (ClickHandler) select_click_handler);
+  window_long_click_subscribe(BUTTON_ID_SELECT, 700,(ClickHandler) select_long_click_handler, select_long_click_release_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN, (ClickHandler) down_click_handler);
   window_single_click_subscribe(BUTTON_ID_UP, (ClickHandler) up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_BACK, (ClickHandler) back_click_handler);
 }
 
 static void menu_squeezebox(int index, void *ctx) {
@@ -77,7 +88,7 @@ static void menu_squeezebox(int index, void *ctx) {
 }
 
 static void windows_squeezebox_load(Window *window){
-  layer_mark_dirty(simple_menu_layer_get_layer(s_simple_menu_layer));
+  // layer_mark_dirty(simple_menu_layer_get_layer(s_simple_menu_layer));
 
   // Initialize the action bar:
   action_bar = action_bar_layer_create();
@@ -95,12 +106,59 @@ static void windows_squeezebox_load(Window *window){
   action_bar_layer_set_icon_animated(action_bar, BUTTON_ID_UP, s_background_bitmap, true);
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ICON_NEXT);
   action_bar_layer_set_icon_animated(action_bar, BUTTON_ID_DOWN, s_background_bitmap, true);
-
-  // simple_menu_layer_destroy(s_simple_menu_layer); 
 }
-static void windows_squeezebox_unload(Window *window)
-{
+static void windows_squeezebox_unload(Window *window){
   action_bar_layer_destroy(action_bar);
+}
+
+static void menu_advance_squeezebox(int index, void *ctx) {
+  window_stack_push(window_advance_squeezebox, true);
+}
+
+static void windows_advance_squeezebox_load(Window *window) {
+  // Although we already defined NUM_FIRST_MENU_ITEMS, you can define
+  // an int as such to easily change the order of menu items later
+  int num_a_items = 0;
+
+  s_advance_squeezebox_first_menu_items[num_a_items++] = (SimpleMenuItem) {
+    .title = "one",
+    .callback = menu_select_callback,
+  };
+  s_advance_squeezebox_first_menu_items[num_a_items++] = (SimpleMenuItem) {
+    .title = "two",
+    .subtitle = "Here's a subtitle",
+    .callback = menu_select_callback,
+  };
+  s_advance_squeezebox_first_menu_items[num_a_items++] = (SimpleMenuItem) {
+    .title = "Third Item",
+    .subtitle = "This has an icon",
+    .callback = menu_select_callback,
+  };
+
+  s_advance_squeezebox_second_menu_items[0] = (SimpleMenuItem) {
+    .title = "Special Item",
+  };
+
+  s_advance_squeezebox_menu_sections[0] = (SimpleMenuSection) {
+    .num_items = NUM_FIRST_MENU_ITEMS,
+    .items = s_advance_squeezebox_first_menu_items,
+  };
+  s_advance_squeezebox_menu_sections[1] = (SimpleMenuSection) {
+    .title = "Yet Another Section",
+    .num_items = NUM_SECOND_MENU_ITEMS,
+    .items = s_advance_squeezebox_second_menu_items,
+  };
+
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  s_advance_squeezebox_menu_layer = simple_menu_layer_create(bounds, window, s_advance_squeezebox_menu_sections, NUM_MENU_SECTIONS, NULL);
+
+  layer_add_child(window_layer, simple_menu_layer_get_layer(s_advance_squeezebox_menu_layer));
+}
+
+static void windows_advance_squeezebox_unload(Window *window) {
+  simple_menu_layer_destroy(s_advance_squeezebox_menu_layer);
 }
 
 static void special_select_callback(int index, void *ctx) {
@@ -149,7 +207,7 @@ static void main_window_load(Window *window) {
   s_first_menu_items[num_a_items++] = (SimpleMenuItem) {
     .title = "Third Item",
     .subtitle = "This has an icon",
-    .callback = menu_select_callback,
+    .callback = menu_advance_squeezebox,
     .icon = s_menu_icon_image,
   };
 
@@ -219,6 +277,7 @@ static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
   windows_squeezebox = window_create();
+  window_advance_squeezebox = window_create();
   window_set_click_config_provider(s_main_window, click_config_provider);
 
   // Set handlers to manage the elements inside the Window
@@ -230,6 +289,11 @@ static void init() {
   window_set_window_handlers(windows_squeezebox, (WindowHandlers) {
     .load = windows_squeezebox_load,
     .unload = windows_squeezebox_unload
+  });
+
+  window_set_window_handlers(window_advance_squeezebox, (WindowHandlers) {
+    .load = windows_advance_squeezebox_load,
+    .unload = windows_advance_squeezebox_unload
   });
 
   // Show the Window on the watch, with animated=true
